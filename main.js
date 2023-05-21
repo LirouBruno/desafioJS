5fc9736643a29433495b16099ddbea2611d7d37e
LirouBruno <brunobezerra0906@gmail.com>
2023-05-21 16:29:30 -0300

    attzinha

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-window.alert('Bem-vindo a melhor casa de apostas.')
+var nome = window.prompt('Qual o seu nome, meu jogador?') // Input do JS
+window.alert('Seja bem-vindo a maior casa de apostas, ' + nome) //Concatenação
 window.confirm('Preparado para sua bet?')
-function showbet(){
-    window.prompt('Qual o seu nome, meu jogador?')
 
-}
 
 
 
