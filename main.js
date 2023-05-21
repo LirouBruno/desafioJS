a8c15f0fcf51572ce706cc41fcf1f17fcd79f348
LirouBruno <brunobezerra0906@gmail.com>
2023-05-21 18:40:51 -0300

    attzinha2

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 var nome = window.prompt('Qual o seu nome, meu jogador?') // Input do JS
-window.alert('Seja bem-vindo a maior casa de apostas, ' + nome) //Concatenação
 window.confirm('Preparado para sua bet?')
-
-
-
-
+document.write(`<h1>Bem-vindo a Bet365, ${nome}</h1>`)
